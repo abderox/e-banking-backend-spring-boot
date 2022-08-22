71dc39fcde32999f0022404af971ed8c96f22945
NaciriAli <alinaciri2002@gmail.com>
2022-08-22 01:14:09 +0100

    initiation du projet

---
+++ src/main/java/com/adria/projetbackend/ProjetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.adria.projetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetBackendApplication.class, args);
+    }
+
+}
